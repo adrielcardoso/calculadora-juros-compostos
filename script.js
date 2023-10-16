cb30ae18ce472df3bc116e2439fdb8dba5f2aa15
Adriel Cardoso <adrielcardoso99@gmail.com>
2023-10-16 12:38:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+function calcularJurosCompostos() {
+    const principal = parseFloat(document.getElementById('principal').value);
+    const taxaJuros = parseFloat(document.getElementById('taxaJuros').value) / 100;
+    const tempo = parseFloat(document.getElementById('tempo').value);
+    
+    const montante = principal * Math.pow(1 + taxaJuros, tempo);
+    
+    document.getElementById('montante').textContent = montante.toFixed(2);
+}
